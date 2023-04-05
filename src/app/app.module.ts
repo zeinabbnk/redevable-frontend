dd15b9c06365056320b7fd719b53db56155f927d
Zainab Benkrimi <zaina.benkrimi43@gmail.com>
2023-04-05 01:00:14 +0000

    redevableFrontEnd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RedevableCreateComponent } from './view/redevables/redevable-create/redevable-create.component';
+import { RedevableListComponent } from './view/redevables/redevable-list/redevable-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from "@angular/forms";
+import {Ng2SearchPipeModule} from "ng2-search-filter";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedevableCreateComponent,
+    RedevableListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        Ng2SearchPipeModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
